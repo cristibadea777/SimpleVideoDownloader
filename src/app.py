e4485eb50c9f4789e0ebed7068f327b678827edf
cristibadea777 <cristibadea777@gmail.com>
2023-08-03 14:30:37 +0300

    terminat API-ul si o interfata React JS

--- src/app.py
+++ src/app.py
@@ -48,4 +48,5 @@ def download():
     return render_template('index.html', error=False)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0') #pentru deploy
