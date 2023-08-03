26d927324e7dbebad7767b3095a0d8b5daebe8bd
cristibadea777 <cristibadea777@gmail.com>
2023-08-03 13:56:36 +0300

    terminat API-ul si o interfata React JS

--- app/react-simple-video-downloader/src/App.jsx
+++ app/react-simple-video-downloader/src/App.jsx
@@ -1,7 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import axios from 'axios'
 
 const App = () => {
+  
+  const [stareDescarcare, setStareDescarcare] = useState('')
+  const [link,            setLink]            = useState('')
+
+  const handlePressOk = () => {
+    setStareDescarcare("Downloading...")
+    
+    const formData = new FormData();
+    formData.append('link', link);
+
+    axios.post('http://127.0.0.1:5000/download/', formData, { responseType: 'blob' })
+    .then(response => {
+      setStareDescarcare("Finished ")
+      const video = window.URL.createObjectURL(new Blob([response.data]));
+      //algoritm de descarcare
+      //creare element ancora <a> ascunsa
+      //hreful setat cu url-ul blob-ului
+      //atributul de download cu numele blob-ului
+      //click artificial pe <a>, incepe descarcarea
+      const a = document.createElement('a');
+      a.href = video;
+      //numele fisierului luat din headerul Filename
+      const filename = response.headers.get('Filename')
+      a.download = filename + '.mp4';
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(video);
+    })
+    .catch(error => {
+      console.error('Error downloading:', error);
+      setStareDescarcare("Error");
+    });
+}
+
+
+  const handleChangeTextInput = (event) => {
+    setLink(event.target.value)
+  } 
+
   return (
     <div style={{backgroundColor: "red", }} className="Container">
       <div className="Header-Container">
@@ -14,6 +56,23 @@ const App = () => {
         </div>
       </div>
       <div className="Body-Container">
+      
+        <div style={{display: "flex", flexDirection: "row", width: "100%", padding: 13, alignItems: "center", justifyContent: "flex-start", paddingLeft: "25%"}}>
+          <h1 style={{marginRight: 33}}>Link: </h1>
+          <input 
+            style={{width: "33%", height: "50%", marginLeft: 33, marginRight: 33, border: 0, outline: 0, borderBottomWidth: 1, borderBottomColor: "black", borderBottomStyle: "solid", fontSize: 25, backgroundColor: "cyan"}}
+            onChange={handleChangeTextInput}
+          />
+          <Button 
+            sx={{backgroundColor: "yellow", fontSize: 25, color: "black", marginLeft: 3}}
+            onClick={handlePressOk}
+          >OK</Button>
+        </div>
+      
+        <div style={{display: "flex", flexDirection: "row", width: "100%", padding: 13, alignItems: "center", justifyContent: "flex-start", paddingLeft: "25%"}}>
+          <h1 style={{marginRight: 33}}>Status:</h1>
+          <h1> {stareDescarcare} </h1>
+        </div>
       
       </div>
     </div>
--- src/app.py
+++ src/app.py
@@ -1,26 +1,47 @@
 from flask import Flask, render_template, make_response, request
 import yt_dlp
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+#pt cross origin 
+#intr-un CORS, doar headerul content-type este expus, celelalte trebuie expuse manual daca vrem sa le folosi
+CORS(app, expose_headers=['Filename'])
+
 @app.route("/", methods=['GET'])
 def home():
-    return render_template('index.html', error=False)
+    return "<h1>Creeaza cerere POST catre API-ul Flask (127.0.0.1:5000/download) cu atributul 'link' in Body ce tine url-ul clipului</h1>"
 
-@app.route("/download", methods=['POST'])
+@app.route("/download/", methods=['POST'])
 def download():
     if request.method == 'POST':
-        url = 'https://www.youtube.com/watch?v=MtN1YnoL46Q'
+
+        url = request.form.get('link')  #ia elementul 'link' din cererea POST
+        
+        if not url:
+            return make_response("Link not provided", 400)
+        
         ydl_opts = {'format': 'best'}
 
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(url, download=False)
+                info_dict = ydl.extract_info(url, download=False)#informatii despre videoclip
+                
+                video_id = info_dict.get("id", None)
+                video_title = info_dict.get('title', None)
+                filename = video_title + " " + "[" + video_id + "]"
+
                 video_url = info_dict['url']
-                response = make_response(ydl.urlopen(video_url).read())
-                response.headers['Content-Type'] = 'video/mp4'
-                response.headers['Content-Disposition'] = 'attachment; filename=video.mp4'
-                return response
+                video_data = ydl.urlopen(video_url).read()
+
+                headers = {
+                    'Content-Type': 'video/mp4',
+                    'Filename': filename 
+                }
+
+                print(headers)
+
+                return make_response(video_data, 200, headers)
         except Exception as e:
             return render_template('index.html', error=True)
 
